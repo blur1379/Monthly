f105144de4a3e90c348a33257b5c451769a7f0a1
mohammad blur <blor1379@gmail.com>
2024-07-02 23:38:13 +0330

    :sparkles: add widget main bundle

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Mohammad Blur on 7/2/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
